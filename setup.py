817265bb6b097141b274019b824dd710d3f0a44f
Hridesh sharma <61771411+hridesh-net@users.noreply.github.com>
2024-09-18 21:05:20 +0530

    git path updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A library to parse PDF, DOCX, and TXT files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    url="https://github.com/hridesh-net/praserlib.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
